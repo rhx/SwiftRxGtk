2d496a286e084ef0fa5039d8b4f46451447e1c4f
Rene Hexel <r.hexel@griffith.edu.au>
2019-04-27 14:14:30 +1000

    Make text Rx property non-optional

--- Sources/RxGtk/TextView+Rx.swift
+++ Sources/RxGtk/TextView+Rx.swift
@@ -16,15 +16,14 @@ import RxCocoa
 public extension Reactive where Base: TextView {
 
     /// Reactive wrapper for the `text` property
-    var text: ControlProperty<String?> { return value }
+    var text: ControlProperty<String> { return value }
 
     /// Reactive value wrapper for the `value` property.
-    var value: ControlProperty<String?> {
-        let source: Observable<String?> = TextBuffer(base.buffer).rx.observe(EntryPropertyName.text)
-        let observer = Binder(base) { (control, value: String?) in
+    var value: ControlProperty<String> {
+        let source: Observable<String> = TextBuffer(base.buffer).rx.observe(EntryPropertyName.text).map { $0 ?? "" }
+        let observer = Binder(base) { (control, value: String) in
             control.buffer.map {
-                let string = value ?? ""
-                TextBufferRef($0).set(text: string, len: CInt(string.utf8.count))
+                TextBufferRef($0).set(text: value, len: CInt(value.utf8.count))
             }
         }
         return ControlProperty(values: source, valueSink: observer)
